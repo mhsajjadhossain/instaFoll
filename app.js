5b83d79d089c7316130e3bea458773dd2d18ed30
M.h Sajjad Hossain Ripon <mail.mhsajjad@gmail.com>
2020-11-26 10:05:23 +0600

    site test

---
+++ app.js
@@ -0,0 +1,86 @@
+let triggerBtn = document.querySelectorAll('[data-trigger="true"]')
+let followBtn = document.querySelectorAll('[data-follow="true"]')
+let firstStep = document.querySelector('.banner_wrap')
+let secondStep = document.querySelector('.follow_button_area')
+let thirdStep = document.querySelector('.get_email_wrap')
+let mainForm = document.querySelector('.main_form')
+let userNameInput = document.querySelector('[data-type="username"]')
+let emailInput = document.querySelector('[data-type="emailAdd"]')
+let counterFlag=1;
+let popupWindow = (url)=>{
+	return window.open(url,'popUpWindow','height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
+}
+
+let alertBtn = document.querySelectorAll('[data-condition]')
+
+alertBtn.forEach(btn=>{
+	btn.addEventListener('click',()=>{
+		$('.popupModal').addClass('hide')
+		$('.modal_overlay').addClass('hide')
+
+		triggerBtn.forEach((btn)=>{
+			btn.addEventListener('click',(e)=>{
+				counterFlag++
+				
+				if(2 === counterFlag){
+					firstStep.classList.add('d-none');
+					secondStep.classList.remove('d-none');
+					
+		
+					counterFlag = 0
+		
+					followBtn.forEach((follow)=>{
+						follow.addEventListener('click',(e)=>{
+							counterFlag++
+							$(follow).siblings().addClass('disabled')
+							let url = follow.getAttribute('data-url-info')
+							popupWindow(url)
+							follow.classList.add('disabled')
+							
+							if(counterFlag >= 4){
+								secondStep.classList.add('d-none');
+								thirdStep.classList.remove('d-none');
+								counterFlag = 0;
+		
+								mainForm.addEventListener('submit',(e)=>{
+									e.preventDefault();
+									let username = userNameInput.value;
+									let emailAddress = emailInput.value;
+									$.get( `https://hooks.zapier.com/hooks/catch/1805654/oe3g3a1/?email=${emailAddress}&name=${username}`);
+									
+									$('.get_email_wrap').addClass('d-none')
+									$('.thank_you_msg').removeClass('d-none')
+									   
+								})
+								
+		
+							}
+						})
+					})
+		
+		
+				}
+				
+			})
+		
+		})
+
+		
+	})
+})
+
+
+
+
+
+
+
+
+
+// $('.btn').on('click',function(){
+// 	var val = $('.first').clone()
+
+// 	$('.output').append(val)
+	
+//  console.log('val :', val);
+// })
